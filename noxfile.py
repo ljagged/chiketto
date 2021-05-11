6cfac354d03ba93e76960a496884e11a18ee1799
Alex Garrett <ljagged@gmail.com>
2021-05-10 21:03:49 -0500

    Added coverage to noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -5,3 +5,10 @@ def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+@nox.session(python=["3.9", "3.8", "3.7"])
+def coverage(session: Session) -> None:
+    """Upload coverage data."""
+    install_with_constraints(session, "coverage[toml]", "codecov")
+    session.run("coverage", "xml", "--fail-under=0")
+    session.run("codecov", *session.posargs)
