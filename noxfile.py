ca24779c2987f8c1bba7be7bf77ebf511430daca
Alex Garrett <ljagged@gmail.com>
2021-05-10 21:09:45 -0500

    Constrained cov to py3.8

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ def tests(session: Session) -> None:
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
 
-@nox.session(python=["3.9", "3.8", "3.7"])
+@nox.session(python="3.8")
 def coverage(session: Session) -> None:
     """Upload coverage data."""
     install_with_constraints(session, "coverage[toml]", "codecov")
